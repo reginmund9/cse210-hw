29a3ae1913aba5062043dadceced49a4aba222a6
reginmund9 <var23004@byui.edu>
2023-09-15 15:00:18 -0600

    w1-last

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("Enter your grade: ");
+        string studentGrade = Console.ReadLine();
+        int studentAnswer = int.Parse(studentGrade);
+
+        
+        if (studentAnswer >= 90)
+        {
+            Console.WriteLine("Your grade is: A");
+        }
+        else if (studentAnswer >= 80)
+        {
+            Console.WriteLine("Your grade is: B");
+        }
+        else if (studentAnswer >= 70)
+        {
+            Console.WriteLine("Your grade is: C");
+        }
+        else if (studentAnswer >= 60)
+        {
+            Console.WriteLine("Your grade is: D");
+        }
+        else 
+        {
+            Console.WriteLine("Your grade is: F");
+        }
+
+        if (studentAnswer >= 70)
+        {
+            Console.WriteLine("You passed!"); 
+        }
+        else
+        {
+            Console.WriteLine("Good luck next time!");
+        }
+    }   
 }
